f425b9dd3e515306c9f86d8ffb65791271a6d3e2
Scott Isitt <isitts@hotmail.com>
2022-06-30 23:13:56 -0700

    Generate shopping list component nested in header directory.

--- recipe-app/src/app/app.module.ts
+++ recipe-app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule,
